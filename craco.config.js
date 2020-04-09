b9d5b0b253d415808602dce82492645f794eba08
Jordan Kintzle <jordan.kintzle@vivedlearning.com>
2020-04-09 15:00:13 -0700

    Installed @craco/craco. Updated scripts in package.json to support craco. Created the craco config file. Commenting out the `webpackImporter: false` craco override for now. Project compiles locally and in docker container.

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  style: {
+    sass: {
+      loaderOptions: {
+        // See https://github.com/webpack-contrib/sass-loader/issues/804
+        // webpackImporter: false,
+        // Prefer Dart Sass ('sass') over 'node-sass' if both are installed
+        implementation: require('sass')
+      }
+    }
+  }
+}
