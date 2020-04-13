342de31cb8a8687b0cd371ab313be34f038455bc
Jordan Kintzle <jordan.kintzle@vivedlearning.com>
2020-04-13 10:36:51 -0700

    Issue is now fixed after adding sassOptions and configuring the loader to include files in the `node_modules` folder. https://github.com/webpack-contrib/sass-loader/issues/822#issuecomment-612888406

--- craco.config.js
+++ craco.config.js
@@ -5,7 +5,9 @@ module.exports = {
         // See https://github.com/webpack-contrib/sass-loader/issues/804
         webpackImporter: false,
         // Prefer Dart Sass ('sass') over 'node-sass' if both are installed
-        implementation: require('sass')
+        implementation: require('sass'),
+        // Telling Sass Loader where to find node_modules. Required on linux to compile succussfully.
+        sassOptions: {includePaths: ['node_modules'] }
       }
     }
   }
